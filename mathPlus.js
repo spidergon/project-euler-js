f7ee49d5f991043665d9f500537c1eb98d64ad76
SpiderGon <spidergon@gmail.com>
2017-12-21 17:16:07 -0300

    problem 11

--- mathPlus.js
+++ mathPlus.js
@@ -11,15 +11,6 @@ const special = {
 		}
 		return factors;
 	},
-	/*isPrime: (n) => {
-		if(n <= 1 || (n > 2 && n % 2 == 0)) { return false; }
-		var i = 3;
-		while (i * i <= n) {
-			if (n % i == 0) { return false; }
-			i += 2;
-		}
-		return true;
-	},*/
 	getPrimes: (limit) => {
 		var arr = [2, 3];
      	var counter = 4;
@@ -60,8 +51,7 @@ const special = {
 		if(s[0] != s[len - 1]) { return false; }
 		return special.isPalindrome(s.substring(1, len - 1));
 	},
-	pythagoreanTriplet: (limit) => {
-		// Returns the pythagorean triplet (a, b, c) where a + b + c = limit
+	pythagoreanTriplet: (limit) => { // Returns the pythagorean triplet (a, b, c) where a + b + c = limit
 		var a = 1, b = 2, c = 0;
 		while(true) {
 			c = limit - a - b;
