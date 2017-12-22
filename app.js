87b6927e9036a7bc7d7fb2ca91c22fb17145f11f
SpiderGon <spidergon@gmail.com>
2017-12-22 15:08:23 -0300

    corrections

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const routes = require('./routes/index');
-const helpers = require('./helpers');
-const errorHandlers = require('./handlers/errorHandlers');
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const routes = require("./routes/index");
+const helpers = require("./helpers");
+const errorHandlers = require("./handlers/errorHandlers");
 
 // create our Express app
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views')); // this is the folder where we keep our pug files
-app.set('view engine', 'pug'); // we use the engine pug, mustache or EJS work great too
+app.set("views", path.join(__dirname, "views")); // this is the folder where we keep our pug files
+app.set("view engine", "pug"); // we use the engine pug, mustache or EJS work great too
 
 // serves up static files from the public folder. Anything in public/ will just be served up as the file it is
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Takes the raw requests and turns them into usable properties on req.body
 app.use(bodyParser.json());
@@ -27,13 +27,13 @@ app.use((req, res, next) => {
 });
 
 // After allllll that above middleware, we finally handle our own routes!
-app.use('/', routes);
+app.use("/", routes);
 
 // If that above routes didnt work, we 404 them and forward to error handler
 app.use(errorHandlers.notFound);
 
 // Otherwise this was a really bad error we didn't expect! Shoot eh
-if (app.get('env') === 'development') {
+if (app.get("env") === "development") {
   /* Development Error Handler - Prints stack trace */
   app.use(errorHandlers.developmentErrors);
 }
